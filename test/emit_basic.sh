1404dea5b633a69f4908352f03eaefbcc3e19b1a
Serhei Makarov <serhei.public@gmail.com>
2014-08-01 12:45:41 -0400

    a couple of expression testcases

--- test/emit_basic.sh
+++ test/emit_basic.sh
@@ -2,3 +2,6 @@
 ./sj -e 'probe insn ($opcode == "div", $fname == "foo") {}'
 ./sj -e 'probe insn ($opcode == "div", $fname != "boring") {}'
 ./sj ./test/emit.good/basic1.sj
+
+./sj -e 'probe insn (($fname == "extra" && $opcode == "mul") || $opcode == "div") {}'
+./sj -e 'probe insn ($fname == "this" ? $opcode == "div" : $opcode == "mul") {}'
