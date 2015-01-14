98bc57c08899233ab2632bc54bcfe8529e52a14b
Serhei Makarov <serhei.public@gmail.com>
2015-01-14 11:16:36 -0500

    winter 2015 rewrite -- sample DR clients :: basics

---
+++ dr-demo/empty.c
@@ -0,0 +1,8 @@
+// empty.c :: does absolutely nothing
+
+#include "dr_api.h"
+
+DR_EXPORT void
+dr_init(client_id_t id)
+{
+}
---
+++ dr-demo/hello.c
@@ -0,0 +1,18 @@
+// hello.c :: print "Hello" and "Goodbye" as the target starts and ends
+
+#include "dr_api.h"
+
+static void event_exit(void); // forward decl
+
+DR_EXPORT void
+dr_init(client_id_t id)
+{
+	dr_fprintf(STDERR, "Hello, World!\n");
+	dr_register_exit_event(event_exit);
+}
+
+static void
+event_exit(void)
+{
+	dr_fprintf(STDERR, "Goodbye, World!\n");
+}
